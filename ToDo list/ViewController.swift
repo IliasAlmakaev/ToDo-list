22f79190990c10eef78382d64f40b722fd5fd4b3
Ilias Almakaev <i.almakaev@gmail.com>
2024-11-19 16:22:16 +0700

    Initial Commit

---
+++ ToDo list/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDo list
+//
+//  Created by Ilyas on 19.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
